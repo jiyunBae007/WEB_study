11426a00359a58f2c3099e791964e02209d0835c
jiyunBae007 <63540652+jiyunBae007@users.noreply.github.com>
2020-09-23 20:25:31 +0900

    add

---
+++ login_POST.php
@@ -0,0 +1,12 @@
+<?php
+    $id=$_POST['id'];
+    $pw=$_POST['pw'];
+    $name=$_POST['name'];
+    $gender=$_POST['gender'];
+
+    echo "회원 정보 <br>";
+    echo "ID: $id <br>";
+    echo "Password: $pw <br>";
+    echo "이름: $name <br>";
+    echo "성별(여/남): $gender <br>";
+ ?>
